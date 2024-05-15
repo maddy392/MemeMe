2d0acd7c1275a292b5ede4eb9220aa465767c9cc
maddy392 <maddy392@gmail.com>
2024-05-15 00:00:20 -0500

    2.0 working

---
+++ MemeMe1.0/MemeCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SentMemesCollectionViewCell.swift
+//  MemeMe1.0
+//
+//  Created by MadhuBabu Adiki on 5/12/24.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeImage: UIImageView?
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MemeMe1.0/MemeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  SentMemesTableViewCell.swift
+//  MemeMe1.0
+//
+//  Created by MadhuBabu Adiki on 5/12/24.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var memeImage: UIImageView?
+    @IBOutlet weak var memeText: UILabel?
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
